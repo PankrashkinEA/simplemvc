5ac85a4d05bd48d245e9da3cbe372b0a7b6761f1
e.pankrashin <e.pankrashin@sitesoft.ru>
2021-07-21 15:20:48 +0500

    Fixed styles

--- src/views/login.php
+++ src/views/login.php
@@ -3,17 +3,24 @@
   <head>
     <meta charset="UTF-8">
     <title>Авторизация и регистрация</title>
-    <link rel="stylesheet" href="/main.css">
+    <link rel="stylesheet" href="styles/login.css">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script> -->
 </head>
 <body>
 
 <!-- форма авторизации -->
 
   <form action="" method="post">
-    <label>Ваше имя</label>
-      <input type="text" name="name" placeholder="Введите своё имя">
-    <button type="submit">Войти</button>
-    
+    <div class="row align-items-center">
+        <div class="col-auto">
+            <label class="sr-only" for="inlineFormInput">Ваше имя</label>
+            <input type="text" class="form-control mb-2" id="inlineFormInput" name="name" placeholder="Введите своё имя">
+        </div>
+        <button type="submit">Войти</button>
+    </div>
 </form>
 </body>
 </html>
